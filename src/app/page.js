948af82f05a40b472cef96e79bdc1457f000e9f9
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-20 09:47:00 +0600

    shadcn setup first time

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      project
+      <Button variant="outline">Click me</Button>
     </div>
   );
 }
