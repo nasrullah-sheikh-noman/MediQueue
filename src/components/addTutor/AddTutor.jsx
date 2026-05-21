c9061852f51a99262ffa6f3fa4d6f3481d7ef662
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-21 14:03:48 +0600

    catch data using form on add tutor form

--- src/components/addTutor/AddTutor.jsx
+++ src/components/addTutor/AddTutor.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -9,8 +10,18 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useState } from "react";
 
 const AddTutor = () => {
+  const [subject, setSubject] = useState("");
+  const [mode, setMode] = useState("");
+
+  const onClick = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    console.log("destination", destination);
+  };
   return (
     <div className="container mx-auto max-w-3xl px-4 mt-6 mb-18">
       <div className="mb-8 text-center">
@@ -20,8 +31,10 @@ const AddTutor = () => {
         </p>
       </div>
 
-      <form className="space-y-6 rounded-2xl border p-6 shadow-sm">
-
+      <form
+        onSubmit={onClick}
+        className="space-y-6 rounded-2xl border p-6 shadow-sm"
+      >
         {/* Tutor Name */}
         <div className="space-y-2">
           <label htmlFor="name" className="font-medium">
@@ -30,6 +43,7 @@ const AddTutor = () => {
 
           <Input
             id="name"
+            name="name"
             type="text"
             placeholder="Enter tutor name"
             required
@@ -42,87 +56,58 @@ const AddTutor = () => {
             Upload Photo
           </label>
 
-          <Input
-            id="photo"
-            type="file"
-            required
-          />
-          
+          <Input id="photo" name="photo" type="file" required />
         </div>
 
         {/* Subject & Teaching Mode */}
         <div className="grid gap-4 md:grid-cols-2">
-
           {/* Subject */}
           <div className="space-y-2">
-            <label className="font-medium" >
-              Subject / Category
-            </label>
+            <label className="font-medium">Subject / Category</label>
 
-            <Select required>
+            <Select required onValueChange={setSubject}>
+              <Input type="hidden" name="subject" value={subject}></Input>
               <SelectTrigger>
                 <SelectValue placeholder="Select subject" />
               </SelectTrigger>
 
               <SelectContent>
-                <SelectItem value="math">
-                  Mathematics
-                </SelectItem>
+                <SelectItem value="math">Mathematics</SelectItem>
 
-                <SelectItem value="physics">
-                  Physics
-                </SelectItem>
+                <SelectItem value="physics">Physics</SelectItem>
 
-                <SelectItem value="chemistry">
-                  Chemistry
-                </SelectItem>
+                <SelectItem value="chemistry">Chemistry</SelectItem>
 
-                <SelectItem value="english">
-                  English
-                </SelectItem>
+                <SelectItem value="english">English</SelectItem>
 
-                <SelectItem value="biology">
-                  Biology
-                </SelectItem>
+                <SelectItem value="biology">Biology</SelectItem>
 
-                <SelectItem value="higher-math">
-                  Higher Math
-                </SelectItem>
+                <SelectItem value="higher-math">Higher Math</SelectItem>
 
-                <SelectItem value="ict">
-                  ICT
-                </SelectItem>
+                <SelectItem value="ict">ICT</SelectItem>
               </SelectContent>
             </Select>
           </div>
 
           {/* Teaching Mode */}
           <div className="space-y-2">
-            <label className="font-medium" >
-              Teaching Mode
-            </label>
+            <label className="font-medium ">Teaching Mode</label>
 
-            <Select required>
+            <Select required onValueChange={setMode}>
+              <Input type="hidden" value={mode} name="mode"></Input>
               <SelectTrigger>
                 <SelectValue placeholder="Select mode" />
               </SelectTrigger>
 
               <SelectContent>
-                <SelectItem value="online">
-                  Online
-                </SelectItem>
+                <SelectItem value="online">Online</SelectItem>
 
-                <SelectItem value="offline">
-                  Offline
-                </SelectItem>
+                <SelectItem value="offline">Offline</SelectItem>
 
-                <SelectItem value="both">
-                  Both
-                </SelectItem>
+                <SelectItem value="both">Both</SelectItem>
               </SelectContent>
             </Select>
           </div>
-
         </div>
 
         {/* Available Days & Time */}
@@ -133,6 +118,7 @@ const AddTutor = () => {
 
           <Input
             id="schedule"
+            name="schedule"
             type="text"
             placeholder="Sun - Thu | 5:00 PM - 8:00 PM"
             required
@@ -141,7 +127,6 @@ const AddTutor = () => {
 
         {/* Hourly Fee & Total Slot */}
         <div className="grid gap-4 md:grid-cols-2">
-
           <div className="space-y-2">
             <label htmlFor="fee" className="font-medium">
               Hourly Fee
@@ -149,6 +134,7 @@ const AddTutor = () => {
 
             <Input
               id="fee"
+              name="fee"
               type="number"
               placeholder="500"
               required
@@ -162,12 +148,12 @@ const AddTutor = () => {
 
             <Input
               id="slot"
+              name="slot"
               type="number"
               placeholder="10"
               required
             />
           </div>
-
         </div>
 
         {/* Start Date */}
@@ -176,11 +162,7 @@ const AddTutor = () => {
             Session Start Date
           </label>
 
-          <Input
-            id="date"
-            type="date"
-            required
-          />
+          <Input id="date" name="date" type="date" required />
         </div>
 
         {/* Institution */}
@@ -191,6 +173,7 @@ const AddTutor = () => {
 
           <Textarea
             id="institution"
+            name="institution"
             placeholder="Example: Dhaka University | 3 years teaching experience"
             required
           />
@@ -204,6 +187,7 @@ const AddTutor = () => {
 
           <Input
             id="location"
+            name="location"
             type="text"
             placeholder="Mirpur, Dhaka"
             required
@@ -215,19 +199,15 @@ const AddTutor = () => {
           <Button
             type="button"
             variant="outline"
-            className="w-full"
+            className="w-full cursor-pointer"
           >
             Cancel
           </Button>
 
-          <Button
-            type="submit"
-            className="w-full"
-          >
+          <Button type="submit" className="w-full cursor-pointer">
             Add Tutor
           </Button>
         </div>
-
       </form>
     </div>
   );
