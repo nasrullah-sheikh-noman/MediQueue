f0549d72d3ed537d969d17dbbbacf30c89b949db
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-21 19:53:39 +0600

    show data in ui

---
+++ src/components/tutor/Tutor.jsx
@@ -0,0 +1,12 @@
+
+
+const Tutor = ({ tutor }) => {
+  console.log("tutor", tutor);
+  return (
+    <div>
+      <h2>{tutor.name}</h2>
+    </div>
+  );
+};
+
+export default Tutor;
--- src/components/tutors/Tutors.jsx
+++ src/components/tutors/Tutors.jsx
@@ -1,12 +1,19 @@
 import axios from "axios";
+import Tutor from "../tutor/Tutor";
 
 const Tutors = async () => {
   const res = await axios.get("http://localhost:5000/tutors");
-  console.log("data", res.data);
+  const data = res.data;
+  console.log("data", data);
   return (
-    <>
-      <div>Tutors page</div>
-    </>
+    <div>
+      <div>All Tutors</div>
+      <div>
+        {
+          data.map((tutor, i) => <Tutor key={i} tutor={tutor}></Tutor>)
+        }
+      </div>
+    </div>
   );
 };
 
