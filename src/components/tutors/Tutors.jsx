05fea7c0133cc84108c936b734c2c9d2f92542ad
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-21 19:43:33 +0600

    get api using axios in tutor page

--- src/components/tutors/Tutors.jsx
+++ src/components/tutors/Tutors.jsx
@@ -1,4 +1,8 @@
-const Tutors = () => {
+import axios from "axios";
+
+const Tutors = async () => {
+  const res = await axios.get("http://localhost:5000/tutors");
+  console.log("data", res.data);
   return (
     <>
       <div>Tutors page</div>
